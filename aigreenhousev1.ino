eccf5bbf6e60f9030ead81f3b5674ee58d2e0727
Kevin Schenkers <kevinschenkers@users.noreply.github.com>
2019-07-11 01:06:32 +0200

    houston we are online!

--- aigreenhousev1.ino
+++ aigreenhousev1.ino
@@ -5,11 +5,54 @@
 #include <dht.h>
 #include <elapsedMillis.h>
 
+#include <Bridge.h>
+#include <Console.h>
+#include <YunClient.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
 dht DHT;
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "boss7"
+#define AIO_KEY         "3358822107454ccabee8322f8e37b35a"
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create a BridgeClient instance to communicate using the Yun's bridge & Linux OS.
+BridgeClient client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'photocell' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish iot_temperature = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperature");
+
+Adafruit_MQTT_Publish iot_humidity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/humidity");
+
+Adafruit_MQTT_Publish iot_soilmoisture = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/soilmoisture");
+
+Adafruit_MQTT_Publish iot_soilmoisturediff = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/soilmoisturediff");
+
+Adafruit_MQTT_Publish iot_ledlights = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/ledlights");
+
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Subscribe iot_onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff");
+
+
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Subscribe iot_waterbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/waterbutton");
 
 #define DHT11_PIN 13
 #define LED_PIN     9
 #define NUM_LEDS    120
+#define FASTLED_ALLOW_INTERRUPTS 0
 
 CRGBArray<NUM_LEDS> leds;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -24,7 +67,7 @@ int temp_i;
 int goalTempAim = 18;
 int goalTempIdeal = 20;
 int goalTemp = 18;
-int soilMoistureLevel = 400;
+int soilMoistureLevel = 800;
 int moistureVal = 0;
 int soilPin = A3;
 int soilPower = 6;
@@ -33,12 +76,26 @@ int RELAY2 = A5;
 int buttonPin = 7;
 int motorState = 0;
 int buttonState = 0;         // variable for reading the pushbutton status
-
+int times_watered = 0;
+int NUMBER_SETS = 4;  
+int soilMoistureDiff = 0;
 unsigned long startTime;
 
 void setup() {
 
   Serial.begin(9600);
+
+
+  Bridge.begin();
+  Console.begin();
+  Console.println(F("Adafruit MQTT demo"));
+
+  // Setup MQTT subscription for onoff feed.
+  mqtt.subscribe(&iot_onoffbutton);
+
+  // Setup MQTT subscription for onoff feed.
+  mqtt.subscribe(&iot_waterbutton);
+  
   delay(500);
 
   startTime = millis();
@@ -65,7 +122,7 @@ void setup() {
   digitalWrite(soilPower, LOW);
   
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(15);
+  FastLED.setBrightness(40);
   FastLED.setMaxPowerInVoltsAndMilliamps(5, 1500);
 
   for (int dot = 0; dot < NUM_LEDS; dot++) {
@@ -88,6 +145,30 @@ void setup() {
 
 void loop() {
 
+// Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+
+  // this is our 'wait for incoming subscription packets' busy subloop
+ Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000))) {
+    // Check if its the onoff button feed
+    if (subscription == &iot_waterbutton) {
+      Serial.print(F("On-Off button: "));
+      Serial.println((char *)iot_waterbutton.lastread);
+      
+      if (iot_waterbutton.lastread == 1) {
+       runMotor();
+      }
+      if (iot_waterbutton.lastread == 0) {
+        digitalWrite(RELAY2, LOW);
+      }
+    }
+  }
+
+  
+
   int chk = DHT.read11(DHT11_PIN);
 
   Serial.print("Temperature = ");
@@ -95,6 +176,31 @@ void loop() {
   Serial.print("Humidity = ");
   Serial.println(DHT.humidity);
 
+
+ if (! iot_temperature.publish(DHT.temperature)) {
+    Console.println(F("Failed"));
+  } else {
+    Console.println(F("OK!"));
+  }
+
+ if (! iot_humidity.publish(DHT.humidity)) {
+    Console.println(F("Failed"));
+  } else {
+    Console.println(F("OK!"));
+  }
+
+  if (! iot_soilmoisturediff.publish((int32_t)soilMoistureDiff)) {
+    Console.println(F("Failed"));
+  } else {
+    Console.println(F("OK!"));
+  }
+
+  if (! iot_soilmoisture.publish((int32_t)moistureVal)) {
+    Console.println(F("Failed"));
+  } else {
+    Console.println(F("OK!"));
+  }
+  
   sensorValue = analogRead(A0);
   Serial.print("Light sensor = ");
   Serial.println(sensorValue);
@@ -103,10 +209,13 @@ void loop() {
 
   buttonState = digitalRead(buttonPin);
 
+  
+
   Serial.print("Button state = ");
   Serial.println(buttonState);
 
   if (buttonState == LOW) {
+
 
     runMotor();
 
@@ -118,11 +227,11 @@ void loop() {
 
   if (DHT.temperature >= goalTempIdeal) {
 
-    if ( sensorValue > 200 )
+    if ( sensorValue > 810 )
     {
-      //digitalWrite(RELAY1, HIGH);
+      digitalWrite(RELAY1, HIGH);
     } else {
-      //digitalWrite(RELAY1, LOW);
+      digitalWrite(RELAY1, LOW);
     }
 
   } else {
@@ -132,22 +241,71 @@ void loop() {
   }
 
   if (sensorValue < 200) {
-
+//if(1==1){
     light_status = "Darkness  ";
+    
 
-    leds(0, 30).fill_solid(CRGB::Green);
-    leds(31, 60).fill_solid(CRGB::Red);
-    leds(61, 90).fill_solid(CRGB::Green);
-    leds(91, 120).fill_solid(CRGB::Red);
+//NUM_LED
+for (int x = 0; x < NUM_LEDS; x++)
+{
+    switch (x % NUMBER_SETS)
+    {
+        case (0):
+        {
+            leds[x] = CRGB::Red;
+            break;
+        }
+        case (1):
+        {
+            leds[x] = CRGB::Blue;
+            break;
+        }
+        case (2):
+        {
+            leds[x] = CRGB::Red;
+            break;
+        }
+        case (3):
+        {
+            leds[x] = CRGB::Red;
+            break;
+        }
+    }
+    
+leds(0, 20).fill_solid(CRGB::Black);
+leds(49, 70).fill_solid(CRGB::Black);
+leds(100, 120).fill_solid(CRGB::Black);
+    
+   // Serial.println(x);
+    FastLED.show();
+    delay(10);
+}
+
+//    leds(0, 30).fill_solid(CRGB::Green);
+//    leds(31, 60).fill_solid(CRGB::Red);
+//    leds(61, 90).fill_solid(CRGB::Green);
+//    leds(91, 120).fill_solid(CRGB::Red);
 
     FastLED.show();
     Serial.println("Darkness detected");
+
+if (! iot_ledlights.publish((int32_t)1)) {
+    Console.println(F("Failed"));
+  } else {
+    Console.println(F("OK!"));
+  }
 
   } else {
 
     light_status = "Daylight";
     fill_solid( leds, NUM_LEDS, CRGB::Black);
     Serial.println("Daylight detected");
+
+    if (! iot_ledlights.publish((int32_t)0)) {
+    Console.println(F("Failed"));
+  } else {
+    Console.println(F("OK!"));
+  }
 
   }
 
@@ -159,13 +317,15 @@ void loop() {
   lcd.print("c");
 
   lcd.setCursor(10, 0);
-  lcd.print((millis() - startTime) / 60000);
-  lcd.print("/");
-  lcd.print("60");
+  lcd.print(60 - (millis() - startTime) / 60000);
+  lcd.print(" | ");
+  lcd.print(times_watered);
 
   if (millis() - startTime >= (60 * 60000)) {
 
+   lcd.clear(); 
    waterPlants();
+  
 
     startTime = millis();
   }
@@ -175,21 +335,32 @@ void loop() {
 
 void waterPlants(){
 
+ int moistureSum = 0;
  moistureSum = readSoil();
 
     Serial.print("Soil Moisture = ");
     Serial.println(moistureSum);
 
+int soil_difference = 0;
+soil_difference = moistureSum - soilMoistureLevel;
+
+soilMoistureDiff = soil_difference;
+
+
+Serial.println(soil_difference);
     if (moistureSum < soilMoistureLevel)
     {
       lcd.setCursor(0, 0);
-      lcd.print("Dry");
-
+      lcd.print("Dry ");
+      lcd.print(soil_difference);
+      
       runMotor();
+      
 
     } else {
       lcd.setCursor(0, 0);
-      lcd.print("Wet");
+      lcd.print("Wet ");
+      lcd.print(soil_difference);
 
       digitalWrite(RELAY2, LOW);
     }
@@ -204,6 +375,8 @@ void runMotor()
   {
     unsigned int interval = 2000; //ftime in ms
     unsigned long startTime = millis();
+
+times_watered++;
 
     do
     {
@@ -221,7 +394,7 @@ int readSoil()
 {
   digitalWrite(soilPower, HIGH);
 
-  delay(10);
+  delay(15000);
 
   moistureVal = analogRead(soilPin);
 
@@ -229,3 +402,29 @@ int readSoil()
 
   return moistureVal;
 }
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Console.print("Connecting to MQTT... ");
+
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Console.println(mqtt.connectErrorString(ret));
+       Console.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+  }
+  Console.println("MQTT Connected!");
+}
+
+
+
+
+
